691dbcee01a9dddbd546e43e98c3b6387bedc892
Rekryt <rekrytkw@gmail.com>
2023-10-19 11:43:09 +0300

    Dependency updates (remove nuxt-purgecss)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,7 @@
 import eslintPlugin from 'vite-plugin-eslint';
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    modules: ['@nuxtjs/tailwindcss', 'nuxt-purgecss', '@kevinmarrec/nuxt-pwa'],
-    purgecss: {
-        enabled: true, // Always enable purgecss or process.env.NODE_ENV === 'production'
-        safelist: ['html', 'body', 'a', 'nuxt-progress', 'is-active'],
-    },
+    modules: ['@nuxtjs/tailwindcss', '@kevinmarrec/nuxt-pwa'],
     vite: {
         plugins: [
             eslintPlugin()
